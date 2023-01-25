3661661d30e028dc1c91890aea3d7971d1cb17b2
Alona <alena2003305@gmail.com>
2023-01-25 13:13:30 +0200

    task done

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-export default async function fetchImages(inputValue, pageNr) {
-  return await axios.get(`https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`).then(response => response.data);
+export const fetchImages = async (inputValue, pageNr) => {
+    return await axios.get(
+      `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+    )
+    .then(response => response.data);
 }
